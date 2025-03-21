4d4270215e8dd736bf9ac62cccee4a3837ba8279
Elwa <elwa.bazaesg@gmail.com>
2025-03-21 12:55:45 -0300

    move main carousel item data to mainCarouselItems

---
+++ app/data/mainCarouselItems.ts
@@ -0,0 +1,27 @@
+export const carouselItems = [
+  {
+    title: 'Test',
+    description: 'Este es el test pequeño y es el ',
+    image: '/images/carousel/image1.webp',
+  },
+  {
+    title: 'Test',
+    description: 'Este es el test un mas largo y es el ',
+    image: '/images/carousel/image2.webp',
+  },
+  {
+    title: 'Test',
+    description: 'Este es el test medianamente largo y es el ',
+    image: '/images/carousel/image3.jpg',
+  },
+  {
+    title: 'Test',
+    description: 'Este es el test casi mas largo que los demas y es el ',
+    image: '/images/carousel/image4.jpg',
+  },
+  {
+    title: 'Test',
+    description: 'Este test es mas largo en texto que todos los demas y es el test ',
+    image: '/images/carousel/image5.jpg',
+  }
+]
--- components/modules/main-carousel.tsx
+++ components/modules/main-carousel.tsx
@@ -7,46 +7,14 @@ import {
   CarouselNext,
   CarouselPrevious,
 } from '@/components/ui/carousel'
-import image1 from '@/public/images/carousel/image1.webp'
-import image2 from '@/public/images/carousel/image2.webp'
-import image3 from '@/public/images/carousel/image3.jpg'
-import image4 from '@/public/images/carousel/image4.jpg'
-import image5 from '@/public/images/carousel/image5.jpg'
-
-const textBoxes = [
-  {
-    title: 'Test',
-    description: 'Este es el test pequeño y es el ',
-    image: image1,
-  },
-  {
-    title: 'Test',
-    description: 'Este es el test un mas largo y es el ',
-    image: image2,
-  },
-  {
-    title: 'Test',
-    description: 'Este es el test medianamente largo y es el ',
-    image: image3,
-  },
-  {
-    title: 'Test',
-    description: 'Este es el test casi mas largo que los demas y es el ',
-    image: image4,
-  },
-  {
-    title: 'Test',
-    description: 'Este test es mas largo en texto que todos los demas y es el test ',
-    image: image5,
-  }
-]
+import { carouselItems } from '@/app/data/mainCarouselItems'
 
 export const MainCarousel = () => (
   <Carousel className="w-auto items-center h-full">
     <CarouselContent className='-ml-4'>
-      {textBoxes.map(({ title, description, image }, index) => (
+      {carouselItems.map(({ title, description, image }, index) => (
         <CarouselItem key={index} className='pl-0'>
-          <Card className="shadow-none rounded-none border-b-4 bg-cover bg-center relative w-full h-svh border-none" style={{ backgroundImage: `url(${image.src})` }}>
+          <Card className="shadow-none rounded-none border-b-4 bg-cover bg-center relative w-full h-svh border-none" style={{ backgroundImage: `url(${image})` }}>
             <CardContent className="absolute left-10 bottom-20 items-center justify-center flex-col bg-bg rounded bg-overlay shadow-md hover:shadow-lg transition-shadow">
               <h2 className="text-3xl font-base text-bg">{title}</h2>
               <p className='text-bg'>{description + index}</p>
