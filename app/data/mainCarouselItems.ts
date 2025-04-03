e8fa8f8d0d3f9617fe0d52fcc6e0d29bf924e132
v1nte <ivinte99@gmail.com>
2025-04-03 12:08:51 -0300

    feat: add pathTo into CarouselItems data

--- app/data/mainCarouselItems.ts
+++ app/data/mainCarouselItems.ts
@@ -3,25 +3,30 @@ export const carouselItems = [
     title: '¿Quiénes somos?',
     description: 'Tu aliado estratégico.',
     image: '/images/carousel/image1.webp',
+    pathTo: '/#about'
   },
   {
     title: 'Agenda con un Consultor',
     description: '¿Quieres extender tus posibilidades?',
     image: '/images/carousel/image2.webp',
+    pathTo: '/consultor'
   },
   {
     title: 'Desarrollo de Proyectos',
     description: '',
     image: '/images/carousel/image3.jpg',
+    pathTo: '/proyectos'
   },
   {
     title: 'Adquisición de equipamiento',
     description: '',
     image: '/images/carousel/image4.jpg',
+    pathTo: '/equipamiento'
   },
   {
     title: 'Intervenciones Temporales',
     description: '',
     image: '/images/carousel/image5.jpg',
+    pathTo: '/intervenciones'
   }
 ]
