d9323b7afb6924bd207e275c20ee560ebb388aa7
v1nte <ivinte99@gmail.com>
2025-06-03 10:39:29 -0400

    chore: remove unused import

--- components/modules/blog-post-card.tsx
+++ components/modules/blog-post-card.tsx
@@ -1,25 +1,25 @@
-'Use client'
-import Link from 'next/link';
-import { BlogPost } from '@/app/data/blogPosts';
-import Image from 'next/image';
-import { Card } from '../ui/card';
+"Use client";
+import Link from "next/link";
+import Image from "next/image";
+import { Card } from "../ui/card";
 
 export const BlogPostCard = ({ post }: { post: BlogPost }) => {
   return (
-    <Card
-      className="border p-4 rounded-lg shadow-md hover:shadow-lg transition-shadow">
-      <Link
-        href={`/blog/${post.slug}`}>
+    <Card className="border p-4 rounded-lg shadow-md hover:shadow-lg transition-shadow">
+      <Link href={`/blog/${post.slug}`}>
         <Image
           src={post.image}
           alt={post.title}
           width={400}
           height={200}
-          className="w-full h-48 object-cover rounded-t-lg" />
+          className="w-full h-48 object-cover rounded-t-lg"
+        />
         <div className="p-4">
           <h2 className="text-xl font-bold">{post.title}</h2>
           <p className="text-gray-500 mt-2">{post.excerpt}</p>
-          <p className="text-sm text-gray-700 mt-4 font-semibold">{post.date}</p>
+          <p className="text-sm text-gray-700 mt-4 font-semibold">
+            {post.date}
+          </p>
         </div>
       </Link>
     </Card>
