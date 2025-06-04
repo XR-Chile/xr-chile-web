1a4f1a2fd1df099af96aca91c9bd3e3ead769b22
v1nte <ivinte99@gmail.com>
2025-06-04 14:47:58 -0400

    style: code format

--- components/modules/main-carousel.tsx
+++ components/modules/main-carousel.tsx
@@ -1,14 +1,14 @@
-"use client"
-import { Card, CardContent } from '@/components/ui/card'
+"use client";
+import { Card, CardContent } from "@/components/ui/card";
 import {
   Carousel,
   CarouselContent,
   CarouselItem,
   CarouselNext,
   CarouselPrevious,
-} from '@/components/ui/carousel'
-import { carouselItems } from '@/app/data/mainCarouselItems'
-import Link from 'next/link'
+} from "@/components/ui/carousel";
+import { carouselItems } from "@/app/data/mainCarouselItems";
+import Link from "next/link";
 
 export const MainCarousel = () => (
   <Carousel
@@ -16,24 +16,26 @@ export const MainCarousel = () => (
       align: "start",
       loop: true,
     }}
-    className="w-auto items-center h-full">
-    <CarouselContent className='-ml-4'>
-      {
-        carouselItems.map(({ title, description, image, pathTo }, index) => (
-          <CarouselItem key={index} className='pl-0'>
-            <Card className="shadow-none rounded-none border-b-4 bg-cover bg-center relative w-full h-svh border-none" style={{ backgroundImage: `url(${image})` }}>
-              <CardContent className="absolute left-10 bottom-20 items-center justify-center flex-col bg-bg rounded bg-overlay shadow-md hover:shadow-lg transition-shadow">
-                <Link href={pathTo} className='text-white'>
-                  <h2 className="text-3xl font-base text-bg">{title}</h2>
-                  <p className='text-bg'>{description}</p>
-                </Link>
-              </CardContent>
-            </Card>
-          </CarouselItem>
-        ))
-      }
+    className="w-auto items-center h-full"
+  >
+    <CarouselContent className="-ml-4">
+      {carouselItems.map(({ title, description, image, pathTo }, index) => (
+        <CarouselItem key={index} className="pl-0">
+          <Card
+            className="shadow-none rounded-none border-b-4 bg-cover bg-center relative w-full h-svh border-none"
+            style={{ backgroundImage: `url(${image})` }}
+          >
+            <CardContent className="absolute left-10 bottom-20 items-center justify-center flex-col bg-bg rounded bg-overlay shadow-md hover:shadow-lg transition-shadow">
+              <Link href={pathTo} className="text-white">
+                <h2 className="text-3xl font-base text-bg">{title}</h2>
+                <p className="text-bg">{description}</p>
+              </Link>
+            </CardContent>
+          </Card>
+        </CarouselItem>
+      ))}
     </CarouselContent>
-    <CarouselPrevious className='left-5 size-16' />
-    <CarouselNext className='right-5 size-16' />
+    <CarouselPrevious className="left-5 size-16" />
+    <CarouselNext className="right-5 size-16" />
   </Carousel>
-)
+);
