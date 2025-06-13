68a72b60a5b64fc3db89f4f0531a95e8fc904fcd
Elwa <elwa.bazaesg@gmail.com>
2025-06-12 21:29:51 -0400

    style: code format & change blog class

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,13 +1,14 @@
 import { blogPosts } from "@/lib/data/blogPosts";
 import { BlogPostCard } from "@/components/modules/blog-post-card";
 
-const titles = "text-6xl font-bold text-center";
+const titles = "text-6xl font-bold text-center p-4 ";
+const background = "shadow-md bg-white "
 
 export default function BlogPage() {
   return (
-    <main className="p-8">
-      <h1 className={titles + " mb-20 mt-16"}>Blog</h1>
-      <div className="w-full max-w-7xl mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mb-20">
+    <main>
+      <h1 className={titles + background + "mb-20 mt-16"}>Blog</h1>
+      <div className="w-full max-w-7xl mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mb-20 p-8">
         {blogPosts.map((post, index) => (
           <BlogPostCard key={`${post.slug}-${index}`} post={post} />
         ))}
